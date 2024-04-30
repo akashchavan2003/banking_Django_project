12aa1cf897dddf091f92329c608224ebe170a163
Akash chavan <aschavan892@gmail.com>
2024-04-30 11:11:38 +0530

    adding the file routers.py made this setting apply

--- manage.py
+++ manage.py
@@ -3,8 +3,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
-  
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking.settings')
